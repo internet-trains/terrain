18a62c01abb27335c38776e84deb6de2bac238b5
Larry Fenn <larry.fenn@gmail.com>
2020-07-06 20:46:36 -0400

    Add 'water mask' feature
    This R script takes a black and white PNG file and outputs a gamescript file that will place canals at the specified white pixels

---
+++ waterfiller/use_png.R
@@ -0,0 +1,79 @@
+# Reads a PNG and spits out tiles based on white/black
+library(tidyverse)
+library(png)
+
+image_path <- file.choose()
+image_data <- readPNG(image_path)
+data <- image_data %>%
+  as.table() %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  filter(Freq != 0) %>%
+  mutate(bigVar1 = as.integer(gsub("[A-Z]", "", Var1)),
+         bigVar1 = ifelse(is.na(bigVar1), 0, bigVar1),
+         smallVar1 = match(gsub("[0-9]", "", Var1), LETTERS),
+         bigVar2 = as.integer(gsub("[A-Z]", "", Var2)),
+         bigVar2 = ifelse(is.na(bigVar2), 0, bigVar2),
+         smallVar2 = match(gsub("[0-9]", "", Var2), LETTERS),
+         x = 26 * bigVar1 + smallVar1,
+         y = 26 * bigVar2 + smallVar2
+  )
+
+# for fun, switch the order of the data 
+# (controls which way the water is 'scanned' in in game)
+data <- data %>%
+  arrange(x, y)
+
+# write to OTTD format
+out_str <- paste0(
+  "require(\"version.nut\")
+class Main extends GSController
+{
+    company_id = 0;
+    constructor()
+    {
+    }
+}
+
+function Main::Save()
+{
+    return {};
+}
+// Load function
+function Main::Load() {
+}
+
+function Main::FillCash(company_id) {
+    if(GSCompany.GetBankBalance(company_id) < 20000000) {
+        GSCompany.ChangeBankBalance(company_id, 1800000000, GSCompany.EXPENSES_OTHER);
+    }
+}
+
+function Main::Start()
+{
+    Sleep(1);
+    company_id = GSCompany.ResolveCompanyID(GSCompany.COMPANY_FIRST);
+    FillCash(company_id);
+
+    local x = [",
+  paste0(data$x-1, collapse = ','), # reverse these if ottd is ccw
+  "];
+    local y = [",
+  paste0(data$y-1, collapse = ','),
+  "];
+
+    for(local i = 0; i < x.len(); i++) {
+        local cur_tile = GSMap.GetTileIndex(x[i], y[i]);
+        if(!(GSTile.IsCoastTile(cur_tile) || GSTile.IsWaterTile(cur_tile))) {
+            GSLog.Warning(i);
+            FillCash(company_id);
+            local mode = GSCompanyMode(company_id);
+            GSTile.DemolishTile(cur_tile);
+            GSMarine.BuildCanal(cur_tile);
+        }
+    }
+}
+")
+
+fileconn <- file("~/Documents/OpenTTD/game/waterplacer/main.nut")
+writeLines(out_str, fileconn)
+close(fileconn)
